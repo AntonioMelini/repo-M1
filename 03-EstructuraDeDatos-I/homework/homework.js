2ebecb92100e2a79640cd083f0a3917b8c2eaae3
AntonioMelini <antoniomelini@hotmail.com>
2022-03-31 20:22:11 -0300

    homework -03

--- 03-EstructuraDeDatos-I/homework/homework.js
+++ 03-EstructuraDeDatos-I/homework/homework.js
@@ -6,7 +6,12 @@
 function nFactorial(n) {
   // devolvé el factorial de n (n!)
   // ej:
-  // el factorial de 3 es 6 (3 * 2 * 1)
+  // el factorial de 3 es  (3 * 2 * 1)
+  //1=1
+  //0=1
+  if(n > -1 && n<2){return 1}
+  else if(n<0){return "no se puede con negativos"}
+  return (n * nFactorial(n-1))
 }
 
 function nFibonacci(n) {
@@ -15,6 +20,33 @@ function nFibonacci(n) {
   // nFibonacci(0) // 0  // el elemento 0 es cero
   // nFibonacci(1) // 1 // el elemento 1 es 1
   // nFibonacci(6) // 1 // el elemento 6 es 8
+  if(n===0){return 0}
+  else if (n>0 && n<3){return 1}
+  return nFibonacci(n-1)+nFibonacci(n-2);
+/*
+6=5+4
+5=4+3
+4=3+2
+3=2+1
+2=1+0
+1=1+0
+0=0
+
+resultados=
+0=0
+1=1
+2=1
+3=2
+4=3
+5=5
+6=8
+
+
+
+
+*/
+
+
 
 }
 
@@ -25,6 +57,20 @@ function nFibonacci(n) {
 // size: Devuelve el número de elementos que contiene la queue.
 
 function Queue() {
+  this.queue=[];
+  Queue.prototype.enqueue=function(elem){
+    this.queue.push(elem);
+  }
+  Queue.prototype.dequeue=function(){
+    if(this.queue.length===0){
+      return undefined;
+    }else {
+      return this.queue.shift();
+    }
+  }
+  Queue.prototype.size=function(){
+    return this.queue.length;
+  }
 
 }
 
